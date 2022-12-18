c4d125880a0220bfc7921637d8e98627018ab208
Vlad A <seralesin@mail.ru>
2022-12-18 11:48:59 +0300

    task15 done

---
+++ task15/task15.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введи число, обозначающее день недели");
+int day=int.Parse(Console.ReadLine()??""); 
+if (day==6 || day==7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
