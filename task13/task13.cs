93df60b97166351c741d44fe7240d8b5158fde22
Vlad A <seralesin@mail.ru>
2022-12-18 11:44:53 +0300

    task13 done

---
+++ task13/task13.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введи число");
+string number= (Console.ReadLine()??"");
+int num=int.Parse(number); 
+int del = Convert.ToInt32(Math.Pow(10,number.Length-3));
+if (num>999)
+{ 
+    Console.WriteLine((num/del)%10);
+}
+else if (num>99)
+    {
+        Console.WriteLine(num%10); 
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
